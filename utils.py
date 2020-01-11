2978e61724eb1ebc1f433ad9a54abc99d2c2ad4c
kenextra <nnoduk@gmail.com>
2020-01-11 17:38:18 -0500

    exercise_three done

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.optimize as op
 # Plot Data
 
 
@@ -79,3 +79,93 @@ def train(X, Y, theta, alpha, num_iters, normalize=False):
         X, Y, theta, alpha, num_iters, normalize)
 
     return theta, J_history, mu, sigma, normalize
+
+
+def sigmoid(z):
+    g = 1 / (1 + np.exp(-z))
+    return g
+
+
+def cost_function(theta, X, y, lambd=0):
+    m, n = X.shape
+    y = y.reshape(m, 1)
+    assert(m == y.shape[0])
+    theta = theta.reshape(n, 1)
+    theta_above = theta[1:, :]
+    theta_x = X.dot(theta)
+    h = sigmoid(theta_x)
+    mul = -1 / m
+    lambd_mul = lambd / (2 * m)
+
+    pos = np.dot(y.T, np.log(h))
+    neg = np.dot((1 - y).T, np.log(1 - h))
+    J = (mul * (pos + neg)) + (lambd_mul * theta_above.T.dot(theta_above))
+    return J.flatten()[0]
+
+
+def gradient(theta, X, y, lambd=0):
+    m, n = X.shape
+    y = y.reshape(m, 1)
+    assert(m == y.shape[0])
+    theta = theta.reshape(n, 1)
+    grad = np.zeros(theta.shape)
+
+    theta_above = theta[1:, :]
+    theta_x = X.dot(theta)
+    h = sigmoid(theta_x)
+
+    grad[:, 0] = (1 / m) * (X[:, 0].T.dot(h - y))
+    grad[1:, :] = (1 / m) * (X[:, 1:].T.dot(h - y)) + \
+        ((lambd / m) * theta_above)
+    return grad
+
+
+def one_vs_all(X, y, num_labels, lambd):
+    m, n = X.shape
+    assert (m == y.shape[0])
+    y = y.reshape(m, 1)
+
+    all_theta = np.zeros((num_labels, n + 1))
+
+    X = np.insert(X, 0, 1, axis=1)
+
+    for c in range(1, num_labels + 1):
+
+        initial_theta = np.zeros((n + 1))
+        # y_val = y == c
+        y_val = np.array([1 if label == c else 0 for label in y])
+        y_val = np.reshape(y_val, (m, 1))
+
+        options = {'maxiter': 100, 'disp': True}
+        result = op.minimize(fun=cost_function,
+                             x0=initial_theta,
+                             args=(X, y_val, lambd),
+                             method='TNC',
+                             jac=gradient,
+                             options=options)
+        theta = result.x
+        all_theta[c - 1, :] = theta
+
+    return all_theta
+
+
+def predict_ova(all_theta, X):
+    X = np.insert(X, 0, 1, axis=1)
+    m = X.shape[0]
+    pred = sigmoid(X.dot(all_theta.T))
+    p = np.argmax(pred, axis=1).reshape(m, 1)
+
+    return p + 1  # added one because the label is from 1-10
+
+
+def display_data(imgData):
+    current = 0
+    pad = 1
+    display_array = -np.ones((pad + 10 * (20 + pad), pad + 10 * (20 + pad)))
+    for i in range(10):
+        for j in range(10):
+            display_array[pad + i * (20 + pad):pad + i * (20 + pad) + 20, pad + j * (20 + pad):pad + j * (20 + pad) + 20] = (imgData[current, :].reshape(20, 20, order="F"))
+            current += 1
+    plt.imshow(display_array, cmap='gray')
+    plt.axis('off')
+    plt.show()
